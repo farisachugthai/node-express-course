3d995cddf8fc51072b95fa3d2dc1e3e72f015416
farisachugthai <farischugthai@gmail.com>
2020-09-12 07:48:47 -0400

    add dynamic GET ROUTE

--- server.js
+++ server.js
@@ -6,11 +6,22 @@ const mockUserData = [
   {name: 'Jill'},
 ]
 
-app.get('/users', function (req, res) {
+/* In Express URLs, words with a colon in front of them are treated as vars.
+ * You can access the value of each variable through req.params
+ * Note: Running this file and going to localhost:8000 won't work
+ * Go to localhost:8000/users/whatever-name-you-want
+ * Then open up the console and see if something logged. For example
+ * `yarn start`
+ * and then directing a browser to
+ * http:localhost:8000/users/mark
+ * will display mark
+ */
+app.get('/users/:id', function (req, res) {
+  console.log(req.params.id)
   res.json({
     success: true,
-    message: 'successfully got users. Nice!',
-    users: mockUserData
+    message: 'got 1 user. Nice!',
+    user: req.params.id
   })
 })
 
